2082aad0bafa5a7234e4daefe1448f8a66a0d863
Henry Unite <unitehenry@gmail.com>
2020-04-15 23:58:47 -0400

    whoami and shorturl

--- api/index.js
+++ api/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 8000;
+const dns = require('dns');
+const bodyParser = require('body-parser');
 
 app.use(cors());
+app.use(bodyParser.json());
 
+// Timestamp
 app.get('/api/timestamp/', (req, res) => {
   const date = new Date();
   res.send({unix: date.getTime(), utc: date.toUTCString()});
@@ -23,6 +27,42 @@ app.get('/api/timestamp/:date', (req, res) => {
   } else {
     res.send({error: 'Invalid Date'});
   }
+});
+
+// Request Header Parser
+app.get('/api/whoami', (req, res) => {
+  const {headers} = req;
+
+  const ip = headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  res.send({
+    ipaddress: ip,
+    language: headers['accept-language'],
+    software: headers['user-agent'],
+  });
+});
+
+// URL Shortener
+app.set('url-id', 0);
+
+app.post('/api/shorturl/new', (req, res) => {
+  const {url} = req.body;
+
+  dns.lookup(url, (err, address) => {
+    
+    if (err) {
+      res.send({error: 'invalid URL'});
+    } else {
+      const urlId = app.get('url-id') + 1;
+      app.set('url-id', urlId);
+
+      app.get(`/api/shorturl/${urlId}`, (reReq, reRes) => {
+        reRes.redirect('https://' + url);
+      });
+
+      res.send({original_url: url, short_url: urlId});
+    }
+  });
 });
 
 app.listen(port, () => console.log(`API listening at ${port}`));
